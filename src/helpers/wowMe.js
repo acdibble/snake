4caac627c1b15506bd08f30ebefae16b8fb68cf2
Andrew Dibble <acdibble@gmail.com>
2019-05-17 23:45:38 -0500

    Remove async/await to try to fix error

--- src/helpers/wowMe.js
+++ src/helpers/wowMe.js
@@ -1,20 +1,18 @@
 const AudioContext = window.AudioContext || window.webkitAudioContext;
 const audioCtx = new AudioContext();
 
-const getFile = async (file) => {
-  const response = await fetch(file);
-  const arrayBuffer = await response.arrayBuffer();
-  return audioCtx.decodeAudioData(arrayBuffer);
-};
+const getFile = file => fetch(file)
+  .then(response => response.arrayBuffer())
+  .then(arrayBuffer => audioCtx.decodeAudioData(arrayBuffer));
 
-const play = async (audioBuffer) => {
-  if (audioCtx.state === 'suspended') await audioCtx.resume();
-  const sampleSource = audioCtx.createBufferSource();
-  sampleSource.buffer = audioBuffer;
-  sampleSource.connect(audioCtx.destination);
-  sampleSource.start();
-  return sampleSource;
-};
+const play = audioBuffer => (audioCtx.state === 'suspended' ? audioCtx.resume() : Promise.resolve())
+  .then(() => {
+    const sampleSource = audioCtx.createBufferSource();
+    sampleSource.buffer = audioBuffer;
+    sampleSource.connect(audioCtx.destination);
+    sampleSource.start();
+    return sampleSource;
+  });
 
 const wowMe = (() => {
   let counter = 0;
